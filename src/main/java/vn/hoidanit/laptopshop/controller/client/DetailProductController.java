76887e6ff1cca9d72bf8d4af64dd234f9b0d930a
Ykkshi2110 <quocbui2110@gmail.com>
2024-07-12 02:19:54 +0700

    complete detail product for client

--- src/main/java/vn/hoidanit/laptopshop/controller/client/DetailProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/DetailProductController.java
@@ -5,12 +5,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.service.ProductService;
+
 
 @Controller
 public class DetailProductController {
-    
+    private final ProductService productService;
+
+    public DetailProductController(ProductService productService){
+        this.productService = productService;
+    }
+
     @GetMapping("/product/{id}")
     public String getMethodName(Model model, @PathVariable long id) {
+        Product myPro = this.productService.fetchProductById(id).get();
+        model.addAttribute("detailPro", myPro);
         return "/client/product/detail";
     }
     
